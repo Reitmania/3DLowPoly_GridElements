6b6834e47827fac9ec4a2648bb688d7bff95be7a
Stefan Reitmann <reiti@guybrush.threepwood>
2020-09-04 09:59:16 +0200

    improved side facing

--- TileAsset_Generator.py
+++ TileAsset_Generator.py
@@ -9,6 +9,8 @@ WIDTH = 1
 RANGE_HEIGHT = .5
 RANGE_OFFSET_IN = .15
 
+
+
 '''
 # Deselect all
 bpy.ops.object.select_all(action='DESELECT')
@@ -46,7 +48,7 @@ def reset_scene():
 
     bpy.ops.object.delete() 
 
-def generate_inside(v2,v3,bm):
+def generate_inside(v2,v3,bm, leftside, rightside):
     def create_step(va,vb):
         tmp_height = uniform(.05, RANGE_HEIGHT)
         tmp_offset = uniform(-RANGE_OFFSET_IN, RANGE_OFFSET_IN)
@@ -57,20 +59,32 @@ def generate_inside(v2,v3,bm):
         bm.faces.new((va, vb, vd, vc))
         return vb.co[2] + tmp_height, vc, vd
 
-    starty = v2.co[1]
+    starty = v2.co[1] #v2 - right, v3 - left
     #first step, starting with v2 and v3
     cur_height, v2, v3 = create_step(v2,v3)
+    rightside.append(v2)
+    leftside.append(v3)
 
     while cur_height + RANGE_HEIGHT < HEIGHT:
         cur_height, v2, v3 = create_step(v2,v3)
+        rightside.append(v2)
+        leftside.append(v3)
 
     #last step
     vc = bm.verts.new((v2.co[0],starty,HEIGHT))
     vd = bm.verts.new((v3.co[0],starty,HEIGHT))
     bm.faces.new((v2, v3, vd, vc))
+    return vc, vd, leftside, rightside
 
-def generate_outside(v2,v3,bm):
-    def create_step(va,vb):
+def generate_outside(va,vb,bm,mode, leftside, rightside):
+    starty = va.co[1]
+    if(mode == 0): #normal
+        vc = bm.verts.new((va.co[0],va.co[1],HEIGHT))
+        vd = bm.verts.new((vb.co[0],vb.co[1],HEIGHT))
+        bm.faces.new((va, vb, vd, vc))
+    elif(mode == 1): #down
+        #first step, starting with v2 and v3
+        #cur_height, v2, v3 = create_step(v2,v3)
         tmp_height = uniform(.05, RANGE_HEIGHT)
         tmp_offset = uniform(-RANGE_OFFSET_IN, RANGE_OFFSET_IN)
         while(va.co[1]+tmp_offset < 0 or va.co[1]+tmp_offset > (WIDTH/2)):
@@ -78,19 +92,8 @@ def generate_outside(v2,v3,bm):
         vc = bm.verts.new((va.co[0],va.co[1]+tmp_offset,va.co[2] + tmp_height))
         vd = bm.verts.new((vb.co[0],vb.co[1]+tmp_offset,vb.co[2] + tmp_height))
         bm.faces.new((va, vb, vd, vc))
-        return vb.co[2] + tmp_height, vc, vd
-
-    starty = v2.co[1]
-    #first step, starting with v2 and v3
-    cur_height, v2, v3 = create_step(v2,v3)
-
-    while cur_height + RANGE_HEIGHT < HEIGHT:
-        cur_height, v2, v3 = create_step(v2,v3)
-
-    #last step
-    vc = bm.verts.new((v2.co[0],starty,HEIGHT))
-    vd = bm.verts.new((v3.co[0],starty,HEIGHT))
-    bm.faces.new((v2, v3, vd, vc))
+    return vc,vd, leftside, rightside
+   
 
 def extrude_face(bm, face, translate_forwards=0.0, extruded_face_list=None):
     new_faces = bmesh.ops.extrude_discrete_faces(bm, faces=[face])['faces']
@@ -111,7 +114,7 @@ def generate_asset():
     bm = bmesh.new()
     #bmesh.ops.create_cube(bm, size=1)
 
-    mode = 1 # 0 - normal, 1 - down, 2 - up
+    mode = 0 # 0 - normal, 1 - down, 2 - up
 
     if(mode == 0):
         # ground plate normal
@@ -125,14 +128,32 @@ def generate_asset():
         y1 = 0
         y2 = -WIDTH/2
 
-    v1 = bm.verts.new((x1,y1,0.0))
+    v1 = bm.verts.new((x1,y1,0.0)) #back right
     v2 = bm.verts.new((x1,y2, 0.0)) # front right
     v3 = bm.verts.new((x2, y2, 0.0)) # front left
-    v4 = bm.verts.new((x2, y1, 0.0))
+    v4 = bm.verts.new((x2, y1, 0.0)) # back left
     bm.faces.new((v1, v2, v3, v4))
 
-    generate_inside(v2,v3,bm)
-    generate_outside(v1,v4,bm)
+    leftside = list()
+    rightside = list()
+    rightside.append(v1)
+    rightside.append(v2)
+    leftside.append(v4)
+    leftside.append(v3)
+
+    vo1,vo2, leftside, rightside = generate_inside(v2,v3,bm, leftside, rightside)    
+    vo3,vo4, leftside, rightside = generate_outside(v1,v4,bm,mode, leftside, rightside)
+
+    rightside.append(vo1)
+    rightside.append(vo3)
+    leftside.append(vo2)
+    leftside.append(vo4)    
+
+    #deckel druff
+    bm.faces.new((vo1, vo2, vo4, vo3))
+    #seiten dran
+    bm.faces.new(rightside)
+    bm.faces.new(leftside)
 
 
 
@@ -190,7 +211,18 @@ def generate_asset():
     ob.location = (0, 0, 0)
     '''
 
+def generate_backplate():
+    bpy.ops.mesh.primitive_plane_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), rotation=(0, 0, 0))
+    obj = bpy.data.objects["Plane"]
+    #select vertex
+    obj = bpy.context.active_object
+    bpy.ops.object.mode_set(mode = 'EDIT') 
+    bpy.ops.mesh.select_mode(type="FACE")
+    bpy.ops.mesh.subdivide(number_cuts=3)
+    bpy.ops.discombobulate.ops()
 
+
+    # add greeble
 
 if __name__ == "__main__":
     # lets build some assets for PBD
@@ -200,4 +232,6 @@ if __name__ == "__main__":
     if start_generator:
         reset_scene()
         obj = generate_asset()
+        generate_backplate()
+
 
